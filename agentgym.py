e9473c0f6099b89222e9e95ddb36e06d702c7dce
ajordan5 <a.jordan6955@gmail.com>
2022-10-26 16:21:58 -0600

    Return vector of ray distances for detected targets

--- agentgym.py
+++ agentgym.py
@@ -7,6 +7,13 @@ class AgentGym:
         self.num_agents = num_agents
         self.num_targets = 5
         self.targets = np.zeros((2,self.num_targets))
+
+        self.target_sense_dist = 0.2
+        self.target_sense_azimuth = np.pi/4
+        self.sense_num_rays = 10
+        self.target_sense_increment = 2*self.target_sense_azimuth/self.sense_num_rays
+        self.target_measurements = np.zeros((self.sense_num_rays, num_agents))
+
         self.speed = 0.3
         self.dt = 0.1
         self.reward = 0     # int32 reward that increments with each step
@@ -34,11 +41,19 @@ class AgentGym:
     def y(self):
         return self.state[1,:]
 
+    @property
+    def full_state(self):
+        # Agent states combined with measurements in one column vector
+        combined = np.concatenate((self.state, self.target_measurements), axis=0).reshape(-1,1)
+        return combined
+
     def step(self, omega):
-        self.state += self.dynamics(omega)*self.dt
+        self.reset_measurements()
+        self.state += self.dynamics(omega)*self.dt # TODO wrap angle. See method used for measurements below
         self.bounce()
+        self.search_targets()
         self.update_reward()
-        return (self.state.astype(np.float32),
+        return (self.full_state.astype(np.float32),
                 np.array(self.reward, np.int32), 
                 np.array(self.done, np.int32))
         
@@ -51,6 +66,7 @@ class AgentGym:
                         [thetadot]])
 
     def bounce(self):
+        # TODO replace this with done flag when they go out of bounds
         for i in range(self.num_agents):
             x_i, y_i, theta_i = self.state[:,i]
             
@@ -71,15 +87,45 @@ class AgentGym:
     def update_reward(self):
         self.reward += 1 # TODO
 
+    def search_targets(self):
+        for agent_i in range(self.num_agents):
+            for target_i in range(self.num_targets):
+                diff = self.targets[:,target_i] - self.state[0:2, agent_i] 
+                dist_to_target = np.linalg.norm(diff)
+                angle_to_target = np.arctan2(diff[1], diff[0])
+                measurement_angle = self.wrap_angle_pi2pi(angle_to_target - self.state[2, agent_i])
+                if(dist_to_target < self.target_sense_dist and abs(measurement_angle) < self.target_sense_azimuth):
+                    self.target_plot = self.ax.scatter(self.targets[0, target_i], self.targets[1, target_i], color='r')
+                    self.add_target_measurement(measurement_angle, dist_to_target, agent_i)
+
     def init_targets(self):
         self.targets = np.random.uniform(size=self.targets.shape)
         self.ax.scatter(self.targets[0,:], self.targets[1,:], color='g')
 
+    def add_target_measurement(self, angle, distance, agent_i):
+        ray_i = int(np.round(angle/self.target_sense_increment))
+        self.target_measurements[ray_i, agent_i] = distance
+
+    def reset_measurements(self):
+        self.target_measurements = np.zeros((self.sense_num_rays, self.num_agents))
+
     def plot(self):
         if self.state_plot:
             self.state_plot.remove()
-        self.state_plot = self.ax.scatter(self.state[0,:], self.state[1,:], color='k')
+        self.state_plot = self.ax.scatter(self.x, self.y, color='k')
         # plt.show()
+
+    @staticmethod
+    def wrap_angle_pi2pi(angle):
+
+        # Wrap from 0 to 2*pi or -2*pi
+        while abs(angle) > 2*np.pi:
+            angle -= 2*np.pi * np.sign(angle)
+
+        if abs(angle) > np.pi:
+            angle = -(2*np.pi - abs(angle)) * np.sign(angle)
+
+        return angle
 
 if __name__ == "__main__":
     # gym = AgentGym(3)
@@ -90,7 +136,7 @@ if __name__ == "__main__":
     gym = AgentGym(1)
     for i in range(100):
         omega  = np.random.normal(0, 1)
-        gym.step(np.array([omega]))
-        # print(gym.state)
+        ret = gym.step(np.array([omega]))
+        print(ret)
         gym.plot()
         plt.pause(0.1)
