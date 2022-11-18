a2256e06f90bc9fcef34d9788617d781f5376cae
ajordan5 <a.jordan6955@gmail.com>
2022-11-18 10:40:11 -0700

    Sense 360 degrees

--- sac.py
+++ sac.py
@@ -176,11 +176,11 @@ if __name__ == '__main__':
     gamma = 0.99
     mdp = TargetAcquisitionEnvironment(1,.99, 1000)
 
-    experiment(alg=alg, mdp=mdp, n_epochs=100, n_steps=5000, n_steps_test=2000)
+    # experiment(alg=alg, mdp=mdp, n_epochs=100, n_steps=5000, n_steps_test=2000)
 
 
-    # agent = Agent.load('saved_models/best_reward')
-    # core = Core(agent, mdp)
-    # core.evaluate(n_episodes=5, render=True)
+    agent = Agent.load('saved_models/model0')
+    core = Core(agent, mdp)
+    core.evaluate(n_episodes=5, render=True)
 
     
